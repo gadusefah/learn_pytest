1d93021738bc541224171595a47904d6297f3273
Gilbert Adu-Sefah <gilbertadusefah@gmail.com>
2020-07-20 17:32:15 -0500

    Chapter 3 - 2 Using Markers

---
+++ tests/sportscar/body/test_body.py
@@ -0,0 +1,11 @@
+from pytest import mark
+
+@mark.body
+class BodyTests:
+    @mark.door
+    def test_body_functions_as_expected(self):
+        assert True
+
+    def test_bumper(self):
+        assert True
+        
---
+++ tests/sportscar/engine/test_engine.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.engine
+def test_engine_functions_as_expected():
+    assert True
